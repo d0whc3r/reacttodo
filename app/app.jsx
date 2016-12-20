4da1617941de876324f22c83c419bf71af3b2cb8
user <user@user>
2016-12-20 23:37:57 +0100

    add redux store

--- app/app.jsx
+++ app/app.jsx
@@ -4,17 +4,25 @@ var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 var TodoApp = require('TodoApp');
 
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(() => {
+    console.log('new state', store.getState());
+});
+
+store.dispatch(actions.addTodo('some add'));
+store.dispatch(actions.setSearchText('yard'));
+store.dispatch(actions.toggleShowCompleted());
+
+
+// Load fundation
 $(document).foundation();
 
 // App scss
 require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
-    <Router history={hashHistory}>
-        <Route path="/" component={TodoApp}>
-            {/*<IndexRoute component={}/>*/}
-        </Route>
-    </Router>,
-
+    <TodoApp/>,
     document.getElementById('app')
 );
