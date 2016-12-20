0f0169d4ea5cad661567f41c0b2316cbb8d2eb21
user <user@user>
2016-12-20 23:24:40 +0100

    update reducers

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -26,4 +26,34 @@ describe('Reducers', () => {
             expect(res).toBe(!state);
         });
     });
+    describe('todosReducer', () => {
+        it('should return new todo', () => {
+            var action = {
+                type: 'ADD_TODO',
+                text: 'some text'
+            };
+            var res = reducers.todosReducer(df([]), df(action));
+
+            expect(res.length).toBe(1);
+            expect(res[0].text).toEqual(action.text);
+        });
+        it('should toggle todo', () => {
+            var action = {
+                type: 'TOGGLE_TODO',
+                id: '123'
+            };
+            var todos = [{
+                id: '123',
+                text: 'some',
+                completed: true,
+                createdAt: 1,
+                completedAt: 122
+            }];
+            var res = reducers.todosReducer(df(todos), df(action));
+
+            expect(res.length).toBe(1);
+            expect(res[0].completed).toEqual(!todos[0].completed);
+            expect(res[0].completedAt).toNotExist();
+        });
+    });
 });
