a264ba8f4e9a0f001e810211792625a47f3d39d9
user <user@user>
2016-12-22 20:10:47 +0100

    credentials env

--- app/firebase/index.jsx
+++ app/firebase/index.jsx
@@ -2,11 +2,11 @@ import firebase from 'firebase';
 
 try {
     var config = {
-        apiKey: "AIzaSyB3FwlSvydba2MwciEPRcGMKOaxRLcvVzQ",
-        authDomain: "todoapp-291c0.firebaseapp.com",
-        databaseURL: "https://todoapp-291c0.firebaseio.com",
-        storageBucket: "todoapp-291c0.appspot.com",
-        messagingSenderId: "388344158794"
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        databaseURL: process.env.DATABASE_URL,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID
     };
     firebase.initializeApp(config);
 }
